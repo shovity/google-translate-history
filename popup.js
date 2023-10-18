37d4f3abf647a2224517e999ffd6a2dfd1a73c51
Shovity <shovity@gmail.com>
2023-10-18 13:58:23 +0700

    add remove history

--- popup.js
+++ popup.js
@@ -4,7 +4,24 @@ chrome.storage.local.get('trans').then(({ trans = [] }) => {
         <div>
             <div>${tran.origin}</div>
             <div>${tran.translate}</div>
+            <div class="remove">&times;</div>
         </div>
         `        
     }).join('')
+
+    window.container.addEventListener('click', ({ target }) => {
+        if (target.className !== 'remove') {
+            return
+        }
+
+        const row = target.parentElement
+        const origin = row.querySelector('div').innerHTML
+
+        row.parentElement.removeChild(row)
+
+        chrome.storage.local.get('trans').then(({ trans = [] }) => {
+            trans = trans.filter((t) => t.origin !== origin)
+            chrome.storage.local.set({ trans })              
+        })
+    })
 })
